dfd10297b79dbd32817c1e284cb1cefb0b268031
Minosoa <minoravelonirina@gmail.com>
2025-11-19 12:27:18 +0300

    feat: table class

---
+++ src/main/java/com/example/demo/exo/Table.java
@@ -0,0 +1,32 @@
+package com.example.demo.exo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Table {
+    private double longeur;
+    private double largeur;
+    private String etat;
+    private String couleur;
+
+    public double getSurface() {
+        return this.longeur * this.largeur;
+    }
+}
+
+// portefeuille
+//couleur
+//porteCarte
+//poids
+//marque
+
+//getMoney
+//addMoney
+//checkMoney
+//addCard
+//lost
